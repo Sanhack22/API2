eec793cf3ff628a39a100b77939d768bb3cd129e
unknown <SenaTarde@ARMAPRRQDFSD224.FormacionSena.RED>
2024-02-28 15:33:06 -0500

    API

---
+++ src/docs/swagger.ts
@@ -0,0 +1,45 @@
+import swaggerJSDoc,{OAS3Definition,OAS3Options} from "swagger-jsdoc";
+
+const swaggerDefinition : OAS3Definition = {
+    openapi: "3.0.3",
+    info:{
+        title:"APIREST",
+        version:"1.0.0"
+    },
+    servers:[
+        {
+            url:"http://localhost:5000",
+
+        },
+    ],
+    components:{
+        schemas:{
+            task:{
+                type:"object",
+                required:["_id","name","description","status"],
+                properties:{
+                    _id:{
+                        type:"string",
+                    },
+                    name:{
+                        type:"string",
+                    },
+                    description:{
+                        type:"string",
+                    },
+                    status:{
+                        type:"boolean"
+                    }
+                }
+            }
+        }
+    }
+}
+
+const swaggerOptions : OAS3Options = {
+    swaggerDefinition,
+    apis:["./src/routes/*ts"]
+
+}
+
+export default swaggerJSDoc (swaggerOptions)
